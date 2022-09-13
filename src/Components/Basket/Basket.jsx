d5da064712ae04efd4dfc1b867ebc4508e980a66
berat ozgen <berat.ozgnn.01@gmail.com>
2022-09-13 21:56:27 +0300

    responsive-1
    responsive-1

--- src/Components/Basket/Basket.jsx
+++ src/Components/Basket/Basket.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {mainContext,useContext} from '../../Context'
 import   './Basket.css'
-import Slider from "react-slick";
 
 
 
@@ -31,94 +30,40 @@ const Basket = () => {
       }
 
 
-      const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        cssEase: "linear"
-      };
-
-
+     
 
 
       if (basket.length === 0) {
-
         return(
           <>
-                 <div className='slider'>
-        <Slider {...settings}>
-          <div>
-              <img className='slider-img' src="https://dummyjson.com/image/i/products/1/4.jpg" alt="" />
-          </div>
-          <div>
-             <img className='slider-img' src="https://dummyjson.com/image/i/products/2/3.jpg" alt="" />
-          </div>
-          <div>
-             <img className='slider-img' src="https://dummyjson.com/image/i/products/7/1.jpg" alt="" />          </div>
-          <div>
-              <img className='slider-img' src="https://dummyjson.com/image/i/products/4/4.jpg" alt="" />          </div>
-          <div>
-              <img className='slider-img' src="https://dummyjson.com/image/i/products/5/2.jpg" alt="" />          </div>
-          <div>
-             <img className='slider-img' src="https://dummyjson.com/image/i/products/6/4.jpg" alt="" />            </div>
-        </Slider>
-      </div>
-
-
             <div className='emptyList'> suanda sepetinizde bir urun yok</div>
           </>
         )
-
-
       }
-
-
-
 
   return (
     <>
-        {/* <Header/>    */}
-
         <div className='basket-component'>
-
             <div className='cost'>Ödiceginiz Miktar: {cost}$</div>
-
-            {basket.map((item) => (
-                <div key={item.id} className="basket-card">
-                <div className="basket-card-img"><img src={item.img[0]} alt="" /></div>
-                <div className="basket-card-title">iphone / {item.name}</div>
-                <div className="basket-card-price">Fiyat : {item.price} $</div>
-                <div className="basket-card-stock">Stock : 100 Adet</div>
-                <div className="basket-card-adet"> {item.amount} Adet</div>
-                <div className="basket-card-buttons">
-                    <button onClick={() => removeBasket(item)}>Sepetten  Çıkar</button>
-                </div>
-                </div>
+                {basket.map((item) => (
+                    <div key={item.id} className="basket-card">
+                      <div className="top">
+                        <div className="basket-card-img"><img src={item.img[0]} alt="" /></div>
+                      </div>
+                      <div className="bottom">
+                          <div className="basket-card-title">iphone / {item.name}</div>
+                          <div className="basket-card-price">Fiyat : {item.price} $</div>
+                          <div className="basket-card-stock">Stock : 100 Adet</div>
+                          <div className="basket-card-adet"> {item.amount} Adet</div>
+                          <div className="basket-card-buttons">
+                            <button onClick={() => removeBasket(item)}>Sepetten  Çıkar</button>
+                     </div>
+                      </div>
+                 
+              </div>
             ))}
-
- 
         </div>
-
-
     </>
   )
 }
-
 export default Basket
-
-
-{/* <div className="basket-card">
-<div className="basket-card-img"><img src="https://dummyjson.com/image/i/products/1/1.jpg" alt="" /></div>
-<div className="basket-card-title">iphone / iphone</div>
-<div className="basket-card-price">Fiyat : 1000 $</div>
-<div className="basket-card-stock">Stock : 100 Adet</div>
-<div className="basket-card-adet"> 100 Adet</div>
-<div className="basket-card-buttons">
-    <button>Sepetten  Çıkar</button>
-</div>
-</div> */}
